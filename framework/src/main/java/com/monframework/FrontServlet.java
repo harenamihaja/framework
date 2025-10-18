eba9e3ed0ba96ce48f4b07c2bb1a82ce58555529
harenamihaja <harenamiti.ralijaona@gmail.com>
2025-10-18 19:18:00 +0300

    sprint1-bis
    sprint 1 bis: url

--- framework/src/main/java/com/monframework/FrontServlet.java
+++ framework/src/main/java/com/monframework/FrontServlet.java
@@ -5,14 +5,30 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import jakarta.servlet.RequestDispatcher;
+
 
 public class FrontServlet extends HttpServlet {
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+   @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        chercherRessource(req, resp);
+    }
 
-        resp.setContentType("text/plain;charset=UTF-8");
-        resp.getWriter().println("URL tapée : " + req.getRequestURI());
+    private void chercherRessource(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String path = req.getRequestURI().substring(req.getContextPath().length());
+        if ("/".equals(path)) {
+            resp.getWriter().println("/");
+            return;
+        }
+
+        boolean resourceExists = getServletContext().getResource(path) != null;
+        if (resourceExists) {
+            RequestDispatcher defaultDispatcher = getServletContext().getNamedDispatcher("default");
+            defaultDispatcher.forward(req, resp);
+        } else {
+            resp.getWriter().println(path);
+        }
     }
 }
